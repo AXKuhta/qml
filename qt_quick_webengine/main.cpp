41ab54011da06264d070af5f1b91a7fa7eea83dd
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2023-12-08 17:10:19 +0800

    Fix hardware acceleration in qt_quick_webengine/
    Test by swapping the url to "chrome://gpu"

--- qt_quick_webengine/main.cpp
+++ qt_quick_webengine/main.cpp
@@ -1,12 +1,13 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-
+#include <QtWebEngine/QtWebEngine>
 
 int main(int argc, char *argv[])
 {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
+    QtWebEngine::initialize();
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
